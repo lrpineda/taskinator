685d538938594384e8f23f1e8e2aa2ef4c58290b
lrpineda <luicks212@gmail.com>
2022-04-09 15:31:59 -0600

    Adding the form for tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,12 @@
-var buttonEl = document.querySelector('#save-task');
+var formEl = document.querySelector("#task-form");
 var tasktoDoEl = document.querySelector('#task-to-do');
 
-var createTaskHandler = function() { 
+var createTaskHandler = function(event) { 
+    event.preventDefault();
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task";
     tasktoDoEl.appendChild(listItemEl);
 };
 
-
-buttonEl.addEventListener('click', createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
